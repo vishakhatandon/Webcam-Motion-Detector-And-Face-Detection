266b7a672dff58d32025e77e028c8511b5d2c8d3
Vishakha <garitandon94@gmail.com>
2025-09-14 00:55:03 +0530

    this is 2025 commit

--- motion__detector/motion_detector.py
+++ motion__detector/motion_detector.py
@@ -1,3 +1,4 @@
+
 import cv2,time,pandas
 from datetime import datetime as dt
 
@@ -11,14 +12,14 @@ while True:
     status=0
     check,frame=video.read()
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    gray=cv2.GaussianBlur(gray,(21,21),0)
+    gray=cv2.GaussianBlur(gray,(22,31),0)
 
     if first_frame is None:
         first_frame=gray
         continue
 
     delta_frame=cv2.absdiff(first_frame,gray)
-    thresh_frame=cv2.threshold(delta_frame,30,255,cv2.THRESH_BINARY)[1]
+    thresh_frame=cv2.threshold(delta_frame,30,245,cv2.THRESH_BINARY)[1]
     thresh_frame=cv2.dilate(thresh_frame,None,iterations=2)
 
     (cnts,_)=cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
