35f5e2132cd41c28cd3bf658fb4438cb27f09d86
Vishakha <garitandon94@gmail.com>
2025-09-14 01:21:28 +0530

    121

--- motion__detector/motion_detector.py
+++ motion__detector/motion_detector.py
@@ -12,7 +12,7 @@ while True:
     status=0
     check,frame=video.read()
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    gray=cv2.GaussianBlur(gray,(22,31),0)
+    gray=cv2.GaussianBlur(gray,(22,71),0)
 
     if first_frame is None:
         first_frame=gray
